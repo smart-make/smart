a4fd228c15ae166238aa411c76c652b5fce93ff3
Duzy Chan <code@duzy.info>
2013-03-02 02:37:50 +0800

    fix: smart.restore: context conflicts

--- tests/tools/multi/test.mk
+++ tests/tools/multi/test.mk
@@ -1,4 +1,4 @@
 discard := $(test-log-visit)
 discard := $(shell echo "$(NAMES)" > names.log)
 $(call smart.test.assert.value,TOOL,test)
-$(call smart.test.assert.value,NAMES, multi test test-1 test-2 android sdk ndk)
+$(call smart.test.assert.value,NAMES, multi test test-1 test-2 android sdk:sdk.apk ndk)
