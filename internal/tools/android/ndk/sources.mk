4f6338a6d8bf04080e2d60c4f96ae1b2e218f6ab
Duzy Chan <code@duzy.info>
2013-03-15 11:43:53 +0800

    optimized for reentrance of load.mk; android/ndk: generate source dependencies

--- internal/tools/android/ndk/sources.mk
+++ internal/tools/android/ndk/sources.mk
@@ -18,17 +18,30 @@ $(call smart~unique,smart~INCLUDES)
 
 smart~INCLUDES := $(patsubst %,-I%,$(smart~INCLUDES:-I%=%))
 
-COMPILE.c   = $(TARGET_CC) -o $$@ $(smart~CFLAGS) $(smart~DEFINES) $(smart~INCLUDES) -c $$<
-COMPILE.c++ = $(TARGET_CXX) -o $$@ $(smart~CFLAGS) $(smart~CXXFLAGS) $(smart~CPPFLAGS) $(smart~DEFINES) $(smart~INCLUDES) -c $$<
+define smart~compile.c
+$(TARGET_CC) \
+  -MMD -MG -MP -MF $$(@:%.o=%.d) -MT $$@ \
+  -o $$@ $(smart~CFLAGS) $(smart~DEFINES) $(smart~INCLUDES) \
+  -c $$<
+endef #smart~compile.c
+
+define smart~compile.c++
+$(TARGET_CXX) \
+  -MMD -MP -MF $$(@:%.o=%.d) -MT $$@ \
+  -o $$@ $(smart~CFLAGS) $(smart~CXXFLAGS) $(smart~CPPFLAGS) \
+  $(smart~DEFINES) $(smart~INCLUDES) -c $$<
+endef #smart~compile.c++
 
 define smart~compile~rules
 $(eval smart~s := $(filter %$2, $(smart~sources)))\
 $(eval smart~o := $(smart~s:%$2=$(TARGET_OBJS)/%.o))\
+$(eval smart~d := $(smart~o:%.o=%.d))\
 $(if $(and $(smart~s),$(smart~o)),$(eval \
   smart~OBJS += $(smart~o)
   $(smart~o) : $(TARGET_OBJS)/%.o : %$2 | $(dir $(sort $(smart~o)))
-	$(COMPILE.$1)
- )$(foreach o,$(smart~o),$(call smart~make~target~dir,$o)))
+	$(call smart~compile.$1)
+ )$(foreach o,$(smart~o),$(call smart~make~target~dir,$o))\
+  $(foreach d,$(smart~d),$(if $(wildcard $d),$(eval -include $d))))
 endef #smart~compile~rules
 
 ifdef  smart~sources
--- load.mk
+++ load.mk
@@ -2,154 +2,18 @@
 #    Copyright (C) 2012, Duzy Chan <code@duzy.info>.
 #    All rights reserved.
 #
-#SHELL := /bin/bash
-smart.me = $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
-smart.root := $(smart.me)
-smart.tooldir = $(smart.root)/internal/tools/$(TOOL)
-smart.stack :=
-smart.list :=
-smart.export :=
-smart.settle_root :=
-smart.context.names = this.% export.% THIS.% EXPORT.% \
-  SCRIPT TOOL TOOL_FILE NAME SRCDIR REQUIRES SUBDIRS MODULES TARGETS \
-  SETTLE_ROOT SETTLE $(smart.context.$(TOOL))
-
-define smart.internal
-$(eval MAKEFILE_LIST := $(filter-out $(lastword $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))
-endef #smart.internal
-
-## Set module context variable.
-## e.g. $(call smart.set,$(NAME),VAR,value)
-define smart.set
-$(eval smart.context.$(strip $2)-$(smart.scripts.$(strip $1)) := $(strip $3))
-endef #smart.set
-
-## Get module context variable.
-## e.g. $(call smart.get,$(NAME),VAR)
-define smart.get
-$(smart.context.$(strip $2)-$(smart.scripts.$(strip $1)))
-endef #smart.get
-
-smart.test.load.before.push :=
-smart.test.load.before.pop :=
-smart.test.load.after.push :=
-smart.test.load.after.pop :=
-smart.test.loaded :=
-
-## Assert variable value
-define smart.test.assert.value
-$(eval \
-  ifndef $1
-    $$(error undefined "$1")
-  else
-    ifneq ("$($(strip $1))","$2")
-      $$(error bad value "$(strip $1)", ("$($1)" != "$2"))
-    endif
-  endif
- )
-endef #smart.test.assert.value
-
-## Assert equal
-define smart.test.assert.equal
-$(eval \
-  ifneq ($1,$2)
-    $$(error ($1 != $2))
-  endif
- )
-endef #smart.test.assert.equal
-
-SM.MK = $(error SM.MK is replaced by SCRIPT)
-SMART.MK = $(SCRIPT)
-SMART.DECLARE := $(smart.root)/declare.mk
-SMART.RULES := $(smart.root)/pend.mk
-
-$(smart.internal)
-ROOT := $(or $(smart.me),.)
-OUT = $(ROOT)/out
-
-ifdef MAKEFILE_LIST
-$(warning You should never try to include this file!)
-endif
-
-smart~error :=
-
-#
-#  @param: smart~fun
-#  
-define smart~defun
-$(eval \
-ifdef smart~fun
-define $(smart~fun)
-$$(eval \
-   smart~error :=
-   smart~result :=
-   include $(smart.root)/funs/$(smart~fun)
-   ifneq ($$(smart~error),)
-     $$$$(error $(smart~fun): $$(smart~error))
-   endif
-  )$$(smart~result)
-endef
-endif
- )
-endef #smart~defun
-
-$(foreach smart~fun,\
-    $(notdir $(filter-out %~,$(wildcard $(smart.root)/funs/smart.*))),\
-    $(smart~defun))
-
-smart~defun :=
-
-.SUFFIXES:
-
-PHONY := modules settle clean
-ROOT.MK := $(wildcard $(ROOT)/sm.mk)
-ifdef ROOT.MK
-  include $(ROOT.MK)
+## Optimize for reentrance in cases like "include file.d".
+ifeq ($(words $(MAKEFILE_LIST)),1)
+  smart.me = $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
+  smart.root := $(smart.me)
+  smart.reentrance :=
 else
-  ROOT.MK := $(or $(wildcard $(ROOT)/smart),$(wildcard $(ROOT)/smart.mk))
-  ifdef ROOT.MK
-    MAKEFILE_LIST += $(ROOT.MK)
-    include $(SMART.DECLARE)
-    include $(ROOT.MK)
-    include $(SMART.RULES)
-  endif #ROOT.MK
-endif #ROOT.MK
+  smart.reentrance := true
+endif
 
-#$(warning info: $(smart.list))
-
-define smart~unique
-$(eval \
-  ifdef $1
-    smart~unique.v := $($1)
-    $1 :=
-    $$(foreach _,$$(smart~unique.v),$$(if $$(filter $$_,$$($1)),,$$(eval $1 += $$_)))
-    $1 := $$(strip $$($1))
-    smart~unique.v :=
-  endif
- )
-endef #smart~unique
-
-define smart~rules
-$(smart.restore)$(eval \
-  include $(smart.root)/rules.mk
- )
-endef #smart~rules
-
-$(foreach @script,$(smart.list),$(smart~rules))
-
-smart~rules :=
-
-.DEFAULT_GOAL := modules
-
-###############################
-# TODO: using these to replace smart.context
-foo:  FOO = foo
-foo1: FOO += foo1
-foo2: FOO += foo2
-
-FOOBAR = $(warning $@)
-foo: ~ = $(FOOBAR)
-
-foo:  foo1 ; @echo $@: $(FOO)$~
-foo1: foo2 ; @echo $@: $(FOO)
-foo2:	   ; @echo $@: $(FOO)
+ifneq ($(smart.reentrance),true)
+  MAKEFILE_LIST :=
+  include $(smart.root)/run.mk
+else
+  $(info smart: reentrance)
+endif
