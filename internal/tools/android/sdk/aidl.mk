bab9a95bd1c4b8a3fa80129ab8e2f6b68850f92e
Duzy Chan <code@duzy.info>
2013-05-13 10:50:55 +0800

    fixed aidl building

--- internal/tools/android/sdk/aidl.mk
+++ internal/tools/android/sdk/aidl.mk
@@ -5,14 +5,14 @@
 #
 
 define smart~rule
-  SOURCES.java_from_aidl := @$(OUT)/$(NAME)/sources/.list
-  $(OUT)/$(NAME)/classes/.list : $(OUT)/$(NAME)/sources/.list
-  $(OUT)/$(NAME)/sources/.list : $(SOURCES.aidl)
+  SOURCES.java_from_aidl := @$(OUT)/$(NAME)/.sources_aidl
+  $(OUT)/$(NAME)/.classes : $(OUT)/$(NAME)/.sources_aidl
+  $(OUT)/$(NAME)/.sources_aidl : $(SOURCES.aidl)
 	@mkdir -p "$$(@D)"
 	@for f in $$^ ; do echo "aidl $$$$f"; \
 	$(ANDROID.aidl) -I"$(SRCDIR)/src" -p"$(ANDROID_PREPROCESS_IMPORT)" \
 	-o"$(OUT)/$(NAME)/sources" -b $$$$f ; done
-	@find "$$(@D)" -type f -name '*.java' > $$@
+	@find "$(OUT)/$(NAME)/sources" -type f -name '*.java' > $$@
 endef #smart~rule
 
 $(eval $(smart~rule))
