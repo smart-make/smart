c7a5daa7a2de988ad850d8589ebf362d9d9288c0
Duzy Chan <code@duzy.info>
2015-12-21 19:09:09 +0800

    gcc: using -MM to generate .d files

--- load/internal/tools/gcc/declare.mk
+++ load/internal/tools/gcc/declare.mk
@@ -1,21 +1,25 @@
 # G_GNUC_WARN_UNUSED_RESULT
 # -Werror=format  -->  __attribute__((__sentinel__)) (e.g. G_GNUC_NULL_TERMINATED)
-GCC_Werrors := \
-  -Werror=declaration-after-statement \
-  -Werror=format \
-  -Werror=format-extra-args \
-  -Werror=implicit-function-declaration \
-  -Werror=nested-externs \
-  -Werror=pointer-sign \
-  -Werror=return-type \
-  -Werror=switch \
-  -Werror=uninitialized \
-  -Werror=unused-function \
-  -Werror=unused-variable \
-  -Werror=unused-result \
+GCC_Werrors := $(addprefix -Werror=,\
+  declaration-after-statement \
+  format \
+  format-extra-args \
+  implicit-function-declaration \
+  nested-externs \
+  pointer-sign \
+  return-type \
+  switch \
+  uninitialized \
+  unused-function \
+  unused-variable \
+  unused-result \
+  int-to-pointer-cast \
+  enum-compare \
+  declaration-after-statement \
+  )
 
 CC := gcc
-CFLAGS := $(GCC_Werrors) -Werror=declaration-after-statement
+CFLAGS := $(GCC_Werrors)
 CXXFLAGS := $(GCC_Werrors)
 ARFLAGS := cru
 LDFLAGS :=
@@ -23,6 +27,8 @@ LIBADD :=
 LOADLIBS :=
 
 ifeq ($(shell uname),Linux)
+
+EXEEXT :=
 
 else # uname == Linux
 LOADLIBS += \
@@ -45,6 +51,5 @@ LOADLIBS += \
   -lnetapi32\
   -lmpr\
 
-endif # uname != Linux
-
 EXEEXT := .exe
+endif # uname != Linux
--- load/internal/tools/gcc/sources.mk
+++ load/internal/tools/gcc/sources.mk
@@ -7,6 +7,7 @@ SOURCES.C := $(filter %.C, $(SOURCES))
 SOURCES.c := $(filter %.c, $(SOURCES))
 SOURCES.s := $(filter %.s, $(SOURCES))
 SOURCES.S := $(filter %.S, $(SOURCES))
+SOURCES.go := $(filter %.go, $(SOURCES))
 
 OBJECTS := \
   $(SOURCES.cc:%.cc=%.o)\
@@ -24,14 +25,50 @@ $(call smart~unique,CXXFLAGS)
 INCLUDES := $(patsubst %,-I%,$(INCLUDES:-I%=%))
 INCLUDES := $(patsubst %,-I%,$(INCLUDES:-I%=%))
 
-COMPILE.c++ = $(CXX) -o $$@ $(strip $(CXXFLAGS) $(DEFINES) $(INCLUDES) -c) $$<
-COMPILE.go = $(GCCGO) -o $$@ $(strip $(GOFLAGS) $(DEFINES) $(INCLUDES) -c) $$<
-COMPILE.c = $(CC) -o $$@ $(strip $(CFLAGS) $(DEFINES) $(INCLUDES) -c) $$<
-COMPILE.s = $(AS) -o $$@ $(strip $(ASFLAGS) $(INCLUDES)) $$<
+define COMPILE.c++
+  $(CXX) -o $$@ $(CXXFLAGS) $(DEFINES) $(INCLUDES) -c $$<
+endef
+
+define COMPILE.c++.d
+  $(CXX) -MMD -MP -MF $$@ -MT $$(@:%.d=%.o) $(CXXFLAGS) $(DEFINES) $(INCLUDES) -c $$<
+endef
+
+define COMPILE.go
+  $(GCCGO) -o $$@ $(GOFLAGS) $(DEFINES) $(INCLUDES) -c $$<
+endef
+
+define COMPILE.go.d
+  $(GCCGO) -MMD -MP -MF $$@ -MT $$(@:%.d=%.o) $(GOFLAGS) $(DEFINES) $(INCLUDES) -c $$<
+endef
+
+define COMPILE.c
+  $(CC) -o $$@ $(CFLAGS) $(DEFINES) $(INCLUDES) -c $$<
+endef
+
+define COMPILE.c.d
+  $(CC) -MMD -MP -MF $$@ -MT $$(@:%.d=%.o) $(CFLAGS) $(DEFINES) $(INCLUDES) -c $$<
+endef
+
+define COMPILE.s
+  $(AS) -o $$@ $(ASFLAGS) $(INCLUDES) $$<
+endef
+
+define COMPILE.s.d
+  $(AS) -MMD -MP -MF $$@ -MT $$(@:%.d=%.o) $(ASFLAGS) $(INCLUDES) $$<
+endef
 
 $(eval $(SOURCES.cc:%.cc=%.o):%.o:%.cc	  ; $(COMPILE.c++))
 $(eval $(SOURCES.cpp:%.cpp=%.o):%.o:%.cpp ; $(COMPILE.c++))
 $(eval $(SOURCES.go:%.go=%.o):%.o:%.go	  ; $(COMPILE.go))
 $(eval $(SOURCES.c:%.c=%.o):%.o:%.c	  ; $(COMPILE.c))
-$(eval $(SOURCES.s:%.s=%.o):%.o:%.s	  ; $(COMPILE.c))
-$(eval $(SOURCES.S:%.S=%.o):%.o:%.S	  ; $(COMPILE.c))
+$(eval $(SOURCES.s:%.s=%.o):%.o:%.s	  ; $(COMPILE.s))
+$(eval $(SOURCES.S:%.S=%.o):%.o:%.S	  ; $(COMPILE.s))
+
+$(eval $(SOURCES.cc:%.cc=%.d):%.d:%.cc	  ; $(COMPILE.c++.d))
+$(eval $(SOURCES.cpp:%.cpp=%.d):%.d:%.cpp ; $(COMPILE.c++.d))
+$(eval $(SOURCES.go:%.go=%.d):%.d:%.go	  ; $(COMPILE.go.d))
+$(eval $(SOURCES.c:%.c=%.d):%.d:%.c	  ; $(COMPILE.c.d))
+$(eval $(SOURCES.s:%.s=%.d):%.d:%.s	  ; $(COMPILE.s.d))
+$(eval $(SOURCES.S:%.S=%.d):%.d:%.S	  ; $(COMPILE.s.d))
+
+$(foreach d,$(OBJECTS:%.o=%.d),$(eval -include $d))
